756441fa9670e1b9a7cac8b9d5145f7f09ae62d5
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-05 21:58:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,9 +41,8 @@ class Song
   def self.new_from_filename(mp3)
     filename = mp3
       data = filename.split(" - ")
-      song_name = data[1]
       artist_name = data[0]
-      
+      song_name = data[1]
     song = self.new 
     song.name = song_name
     song.artist_name = artist_name
